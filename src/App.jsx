204cf113056c3c5575c4b25f70b8b997561dd794
Ilyos Jabborov <dzabboroviles5@gmail.com>
2023-12-08 12:17:43 +0500

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { Header } from "./components/Header/Header";
-import { Section } from "./components/Section/Section";
-import style from "./App.module.css";
-import { user } from "./data/data";
-
 function App() {
   return (
     <div>
-      <Header
-        firstName={user.firstName}
-        lastName={user.lastName}
-        role={user.role}
-      />
-      <Section user={user} />
+     test
     </div>
   );
 }
