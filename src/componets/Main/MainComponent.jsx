69559a19aeb72ac494e2fb42345100a05f089cd9
Ilyos Jabborov <dzabboroviles5@gmail.com>
2023-12-26 18:25:42 +0500

    clean

--- src/componets/Main/MainComponent.jsx
+++ src/componets/Main/MainComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import styles from "./MainComponent.module.css";
 import { ProductCard } from "./ProductCard";
 import { Loader } from "./Loader";
